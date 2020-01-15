a862755bccbafc2e60a137dc86d7f97715e4ea61
kobierzynmaster <58975255+kobierzynmaster@users.noreply.github.com>
2020-01-15 11:25:19 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,24 +1,9 @@
-package testcases;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class HelloWorld {
 
-	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:\\Webdriver\\chromedriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://google.pl/");
-		driver.findElement(By.name("q")).sendKeys("Hello webdriver |!|");
-		driver.findElement(By.id("gsr")).click();
-		
-		Thread.sleep(5000);
-		
-		driver.close();
-		driver.quit();
-
-	}
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
 
 }
+
